9864f71f60f0157cbc4100bdfc6c31da05087a7c
FanJie <914456697@qq.com>
2019-02-16 15:58:03 +0800

    2019年2月16日15:57:17 @范捷   添加用户注册功能

--- src/com/fhelp/servlet/RegisteServlet.java
+++ src/com/fhelp/servlet/RegisteServlet.java
@@ -1,6 +1,8 @@
 package com.fhelp.servlet;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,8 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fhelp.base.BaseBean;
+import com.fhelp.base.User;
 import com.fhelp.service.UserService;
 import com.fhelp.service.impl.UserServiceImpl;
+import com.fhelp.util.JavaBaseUtil;
 import com.google.gson.Gson;
 
 public class RegisteServlet extends HttpServlet {
@@ -18,19 +22,48 @@ public class RegisteServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
-		//1.先从表单中获取注册信息
+		// 1.先从表单中获取注册信息
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String psw_comfirm = request.getParameter("psw_comfirm");
-		
-		//2.判断数据是否是有效的并将结果返回给客户端
+
+		// 2.判断数据是否是有效的并将结果返回给客户端
 		Gson gson = new Gson();
 		BaseBean base = new BaseBean();
 		UserService service = new UserServiceImpl();
-		if(service.findUser(username)==null) {
-			//当前
+		User user = new User();
+		if (service.findUser(username) != null) {
+			// 当前用户已经被注册
+			base.setCode(101);
+			base.setMsg("该账号已经被注册！");
+			base.setData(user);
+		} else if (!password.equals(psw_comfirm)) {
+			// 两次输入的密码不相等
+			base.setCode(102);
+			base.setMsg("两次密码输入的不一致");
+			base.setData(user);
+		} else if (!JavaBaseUtil.checkUsername(username)) {
+			base.setCode(103);
+			base.setMsg("只能是以字母开头(不区分大小写)、6-16位、不包含特殊符号（、_.%$#@!)和空格");
+			base.setData(user);
+		} else if (!JavaBaseUtil.checkPassword(password)) {
+			base.setCode(104);
+			base.setMsg("长度为6-16位 但不能是9位以下纯数字且可以包含字母、数字、特殊符号(~!@#$%^&*()_+[]{}|\\;:'\",./<>?)");
+			base.setData(user);
+		}else {
+			//信息正确
+			base.setCode(200);
+			base.setMsg("注册成功");
+			user.setUsername(username);
+			user.setPassword(password);
+			Date date = new Date(System.currentTimeMillis());
+			user.setRegistertime(date);
+			user.setAutograph("这个人是个猪，没写任何东西");
+			base.setData(user);
+			//添加到数据库中
+			service.register(user);
 		}
-		
+		response.getWriter().println(gson.toJson(base));
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ src/com/fhelp/util/JavaBaseUtil.java
@@ -0,0 +1,56 @@
+package com.fhelp.util;
+
+import com.fhelp.base.PswType;
+
+public class JavaBaseUtil {
+
+	/**
+	 * 检测用户名是否合法 用户名格式：只能是以字母开头(不区分大小写)、6-16位、不包含特殊符号（、_.%$#@!空格)
+	 * 
+	 * @param username
+	 * @return
+	 */
+	public static boolean checkUsername(String username) {
+		if (username.matches("^[a-zA-Z]\\w{5,15}$") && !username.contains(" ")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 检测密码是否合法 密码格式：长度为6-16位 但不能是9位以下纯数字且可以包含
+	 * 字母、数字、特殊符号(~!@#$%^&*()_+[]{}|\;:'",./<>?)
+	 * 
+	 * 强：包含字母、数字、特殊符号三种 中：只包含字母、数字、特殊符号中任意两种 弱：只包含任意一种
+	 * 
+	 * @param password
+	 * @return
+	 */
+	public static boolean checkPassword(String password) {
+		char arr[] = password.toCharArray();
+		boolean digit, word, symbol, l;
+		boolean flag = true;
+		// 1.判断是否含有数字
+		digit = password.matches("^.*\\d+.*$");
+		// 2.判断是否有字母
+		word = password.matches("^.*\\w+.*$");
+		// 3.判断是否有特殊符号
+		symbol = password.matches(".*[~!@#$%^&*()_+|<>,.?/:;'\\[\\]{}\"]+.*");
+		int len = arr.length;
+		// 4.判断长度是否为6-16
+		l = (len >= 6 && len <= 16) ? true : false;
+		if (l) {
+			// 符合长度
+			if (digit && !word && !symbol) {
+				if (len < 9) {
+					// 不能9位以下纯数字
+					flag = false;
+				}
+			}
+		} else {
+			// 长度不符合
+			flag = false;
+		}
+		return flag;
+	}
+}
